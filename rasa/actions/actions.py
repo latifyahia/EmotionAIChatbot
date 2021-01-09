3ed73a2b29e598484eb6f532192112f380abb7d4
latif <latifyahia98@gmail.com>
2021-01-09 05:55:15 +0000

    made alot of chnages to the bot and now have it communicatig to the user where it will ask users name and reply with to the user with their name and ask about how theyre day is etc etc. Im only currently working on the happy path and have no started on other emotions yet

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -7,21 +7,71 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class InformationAction(Action):
+
+    def name(self) -> Text:
+        return "name_form"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        required_slots = ["name"]
+
+        for slot_name in required_slots:
+            if tracker.slots.get(slot_name) is None:
+                return[SlotSet("requested_slots"), slot_name]
+
+        return[SlotSet("requested_slot"), None]
+
+class ActionSubmit(Action):
+
+
+    def name(self) -> Text:
+        return "action_submit"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(template="utter_ask_name", Name=tracker.get_slot("name"))
+
+        return []
+
+class InformationAction2(Action):
+
+    def name(self) -> Text:
+        return "celebration_form"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        required_slots = ["celebration"]
+
+        for slot_name in required_slots:
+            if tracker.slots.get(slot_name) is None:
+                return[SlotSet("requested_slots"), slot_name]
+
+        return[SlotSet("requested_slot"), None]
+
+class ActionSubmit2(Action):
+
+
+    def name(self) -> Text:
+        return "action_submit_good_day"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(template="utter_day_response_happy", Celebration=tracker.get_slot("celebration"))
+
+        return []
